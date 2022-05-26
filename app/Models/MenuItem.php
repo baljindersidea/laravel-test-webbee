04b3e5797c2fab79232ab5f50862f3097f31059a
Baljinder Singh <baljinders@ideafoundation.in>
2022-05-26 13:44:21 +1000

    Menu items returned

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -8,5 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class MenuItem extends Model
 {
+    protected $parentColumn = 'parent_id';
 
+    public function parent()
+    {
+        return $this->belongsTo(MenuItem::class,$this->parentColumn);
+    }
+
+    public function childs()
+    {
+        return $this->hasMany(MenuItem::class, $this->parentColumn);
+    }
+
+    public function children()
+    {
+        return $this->childs('children')->with('childs');
+    }
 }
